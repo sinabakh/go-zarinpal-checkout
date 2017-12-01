a3e1574d60461ebf94ab85a3dec7134f5d1a68f5
Sina Bakhtiari <sinabakh44@live.com>
2017-12-01 21:49:54 +0330

    Add Code Examples For Doc
    Add code examples to be added to docs.

---
+++ example_test.go
@@ -0,0 +1,36 @@
+package zarinpal_test
+
+import (
+	"log"
+
+	"github.com/sinabakh/go-zarinpal-checkout"
+)
+
+func ExampleNewZarrinpal() {
+	zarinPay, err := zarinpal.NewZarrinpal("XXXX-XXXX-XXXX-XXXX", false)
+	if err != nil {
+		log.Fatal(err)
+	}
+	zarinPayTest, err := zarinpal.NewZarrinpal("XXXX-XXXX-XXXX-XXXX", true)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(zarinPay)
+	log.Println(zarinPayTest)
+}
+
+func ExampleZarinpal_NewPaymentRequest() {
+	zarinPay, err := zarinpal.NewZarrinpal("XXXX-XXXX-XXXX-XXXX", true)
+	if err != nil {
+		log.Fatal(err)
+	}
+	paymentURL, authority, statusCode, err := zarinPay.NewPaymentRequest(100, "http://localhost:3000", "Test", "", "")
+	if err != nil {
+		if statusCode == -3 {
+			log.Println("Amount is not accepted in banking system")
+		}
+		log.Fatal(err)
+	}
+	log.Println(authority)  // Save authority in DB
+	log.Println(paymentURL) // Send user to paymentURL
+}
